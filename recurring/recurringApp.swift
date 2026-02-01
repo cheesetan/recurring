31d2053f3f4d3cff516bc131fc913ed2feb3cc7b
Tristan <github@tristanchay.com>
2026-02-02 01:06:36 +0800

    Initial Commit

---
+++ recurring/recurringApp.swift
@@ -0,0 +1,32 @@
+//
+//  recurringApp.swift
+//  recurring
+//
+//  Created by Tristan Chay on 2/2/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct recurringApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
